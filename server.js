0ac4a6b76127abba486ef132874cb6196d1d3486
GPTTest123 <132597746+GPTTest123@users.noreply.github.com>
2024-07-28 00:42:48 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-require('dotenv').config();
+require('dotenv').config(); // Load environment variables from .env file
 
 app.use(express.static(path.join(__dirname, 'public')));
 
